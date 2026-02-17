a15ef2fbf41f024c7cb1387be22878fdeb238488
Sushanth Reddy <suri868877@gmail.com>
2026-02-17 14:58:43 +0530

    new commits

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",        // ⭐ VERY IMPORTANT (creates /out folder)
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
